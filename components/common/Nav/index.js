702d42116a65e20090a5ac7783a9cf038af8e46a
lucas jin <lucas1004jx@gmail.com>
2020-01-17 00:17:04 +0800

    add link tag to menu

--- components/common/Nav/index.js
+++ components/common/Nav/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import Link from 'next/link';
 import Indicator from '../Indicator';
 import './nav.scss';
 
 const Nav = () => {
-  const menuItems = ['delete', 'create new index', 'change target index', 'import data', 'change search index'];
+  //const menuItems = ['delete', 'create new index', 'change target index', 'import data', 'change search index'];
+  const menuItems = [
+    {
+      menuItem: 'delete',
+      link: '/'
+    },
+    {
+      menuItem: 'create new index',
+      link: '/'
+    },
+    {
+      menuItem: 'change target index',
+      link: '/'
+    },
+    {
+      menuItem: 'import data',
+      link: '/'
+    },
+    {
+      menuItem: 'change search index',
+      link: '/'
+    }
+  ];
   const renderNavBar = () => menuItems.map((item, i) => (
-    <ul key={item}>
-      <li>
-        <span><Indicator num={i + 1} /></span>{item}
-      </li>
+    <ul key={item.menuItem}>
+      <Link href={item.link} passHref>
+        <a>
+          <li>
+            <span><Indicator num={i + 1} /></span>{item.menuItem}
+          </li>
+        </a>
+      </Link>
     </ul>
   ));
   return (
